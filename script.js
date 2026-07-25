7b676492ae199b9939c86c46df3a21c8197c8df4
pooja-bisht1 <puuzzaaa18@gmail.com>
2026-07-25 11:15:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,37 +1,37 @@
 const slides = [
 
 {
-image:"images/image1.png",
+image:"https://res.cloudinary.com/m6uaa6eo/image/upload/v1784957340/image1_aefnp9.jpg",
 title:"वेळेत निवारण",
 text:"आपल्या सर्व समस्यांचे निवारण ठराविक वेळेत केले जाईल."
 },
 
 {
-image:"images/image7.png",
+image:"https://res.cloudinary.com/m6uaa6eo/image/upload/v1784957341/image7_kwhkgm.jpg",
 title:"स्वच्छता अभियान",
 text:"स्वच्छ शहर, सुंदर शहर हेच आमचे ध्येय."
 },
 
 {
-image:"images/image3.png",
+image:"https://res.cloudinary.com/m6uaa6eo/image/upload/v1784957340/image3_iccewo.jpg",
 title:"पाणी पुरवठा",
 text:"नागरिकांना नियमित व सुरक्षित पाणी पुरवठा."
 },
 
 {
-image:"images/image4.png",
+image:"https://res.cloudinary.com/m6uaa6eo/image/upload/v1784957341/image4_srvv92.png",
 title:"वृक्षारोपण",
 text:"हरित चिखलीसाठी वृक्षारोपण अभियान."
 },
 
 {
-image:"images/image5.png",
+image:"https://res.cloudinary.com/m6uaa6eo/image/upload/v1784957341/image5_sv4mjs.jpg",
 title:"रस्ता विकास",
 text:"उत्तम रस्ते, उत्तम सुविधा."
 },
 
 {
-image:"images/image6.png",
+image:"https://res.cloudinary.com/m6uaa6eo/image/upload/v1784957341/image6_o3ueg5.jpg",
 title:"नागरिक सेवा",
 text:"पारदर्शक व जलद नागरिक सेवा."
 }
